538f4a6110138ada3aebc87e2b9bdb4ce888fd4a
Edwin van Poeijer <edwin@vanpoeijer.com>
2013-02-09 16:12:22 +0100

    cross platform for unique identifier

---
+++ ScoreoidX/ScoreoidX/Classes/CrossPlatform.h
@@ -0,0 +1,20 @@
+//
+//  CrossPlatform.h
+//  quadro
+//
+//  Created by EAA van Poeijer on 24-01-13.
+//
+//
+
+#ifndef quadro_CrossPlatform_h
+#define quadro_CrossPlatform_h
+
+namespace CrossPlatform
+{
+    struct Identifiers
+    {
+    static const char* uniqueID();
+    };
+}
+
+#endif
